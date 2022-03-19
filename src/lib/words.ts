fbdcb8ab400abc3a5215e885c52dc2580ec749f1
Sanjana Palisetti <sanjana.palisetti@gmail.com>
2022-03-19 19:19:28 +0530

    .

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -91,7 +91,7 @@ export const getWordOfDay = () => {
 
   return {
     // solution: localeAwareUpperCase(WORDS[sr]),
-    solution: WORDS[7],
+    solution: WORDS[8],
     solutionIndex: index,
     tomorrow: nextday,
   }
