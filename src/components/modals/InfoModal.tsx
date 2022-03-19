64c7f328a9f4ef29f3660f77a2c48e31f8237889
Sanjana Palisetti <sanjana.palisetti@gmail.com>
2022-03-19 19:40:58 +0530

    info modal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Welcome Bitchass Non Halal Non Virgin Dog Touching Ass Boi" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
@@ -18,53 +18,52 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="N"
           status="correct"
         />
-        <Cell value="E" />
+        <Cell value="I" />
+        <Cell value="H" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="L" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter N is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="O" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="T"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="A" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter T is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
+        <Cell value="W" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="S" />
+        <Cell isRevealing={true} isCompleted={true} value="T" status="absent" />
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter T is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        All the best. I hope you ain't as bad as you are in hiding weed. 
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D351798418167779&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FClasicTrollFace%2F&tbnid=TaLiHFO8ioJ3gM&vet=12ahUKEwjX5r60rNL2AhWDFFkFHTzNAJUQMygNegUIARDXAQ..i&docid=WC_h1mROdK6oGM&w=500&h=500&q=hehe%20meme%20face&hl=en&ved=2ahUKEwjX5r60rNL2AhWDFFkFHTzNAJUQMygNegUIARDXAQ"
           className="underline font-bold"
         >
-          check out the code here
+          #cancelnihal
         </a>{' '}
       </p>
     </BaseModal>
